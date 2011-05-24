397337dbc913ce2257cc8dcf07b50ab325cffb1a
craig <craig@haquarter.de>
2011-05-25 01:43:12 +0200

    add coloring for awesome

--- cbatt.sh
+++ cbatt.sh
@@ -5,9 +5,18 @@
 # 24.05.2011 - Stefan Behte
 
 echo -n '['
+if [ "$1" = "color" ]
+then
+	echo -n '<span color="red">'
+fi
 
 grep -q 1 /sys/class/power_supply/AC/online && echo -n "+"
 acpi | awk '{print $4, $5}' | sed 's@, @/@g' | awk -F: '{print $1 ":" $2}' | tr -d '\n'
+
+if [ "$1" = "color" ]
+then
+	echo -n '</span>'
+fi
 
 echo -n ']'
 
