b2a8a59a1e9e1dfc5d10f26d27c2c530ea0bb11f
craig <craig@haquarter.de>
2011-05-25 02:23:48 +0200

    workarounds/beautifying for 'charging at zero rate' and 'Unknown'

--- cbatt.sh
+++ cbatt.sh
@@ -12,7 +12,21 @@ then
 fi
 
 grep -q 1 /sys/class/power_supply/AC/online && echo -n "+"
-acpi | awk '{print $4, $5}' | sed 's@, @/@g' | awk -F: '{print $1 ":" $2}' | tr -d '\n'
+
+acpiout=$(acpi)
+if [ -n "$(echo -n ${acpiout} | grep 'charging at zero rate - will never fully charge.')" ]
+then
+	echo -n "${acpiout}" | awk '{print $4, $5}' | awk -F',' '{print $1}' | sed -e 's@,@/@g' -e 's@ @@g' | tr -d '\n'
+	echo -n '/charging at zero rate'
+
+elif [ -n "$(echo -n ${acpiout} | grep 'Unknown, ')" ]
+then
+	echo -n "${acpiout}" | awk '{print $4, $5}' | sed -e 's@,@/@g' -e 's@ @@g' | tr -d '\n'
+	echo -n '/unknown'
+
+else
+	echo -n "${acpiout}" | awk '{print $4, $5}' | sed 's@, @/@g' | awk -F: '{print $1 ":" $2}' | tr -d '\n'
+fi
 
 if [ "$1" = "color" ]
 then
