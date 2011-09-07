1d6f55066729c14be3a725e9d283d3de48c06347
etalas <floh.edo@googlemail.com>
2011-09-07 21:00:27 +0200

    FreeBSD support

--- cbatt.sh
+++ cbatt.sh
@@ -15,12 +15,25 @@ else
 	COLOR=false
 fi
 
-if grep -q 1 /sys/class/power_supply/AC/online
-then
-	CHARGING=true
-else
-	CHARGING=false
-fi
+case `uname -s` in
+	Linux)
+		if grep -q 1 /sys/class/power_supply/AC/online
+		then
+			CHARGING=true
+		else
+			CHARGING=false
+		fi
+		;;
+	FreeBSD)
+		# beware of the ^V^I !
+		if acpiconf -i 0 | egrep -q 'State:[\ \	]*discharging'
+		then
+			CHARGING=false
+		else
+			CHARGING=true
+		fi
+		;;
+esac
 
 if $COLOR
 then
@@ -33,23 +46,49 @@ fi
 
 $CHARGING && echo -n "+"
 
-acpiout=$(acpi)
-if [ -n "$(echo -n ${acpiout} | grep 'charging at zero rate - will never fully charge.')" ]
-then
-	echo -n "${acpiout}" | awk '{print $4, $5}' | awk -F',' '{print $1}' | sed -e 's@,@/@g' -e 's@ @@g' | tr -d '\n'
-	echo -n '/charging at zero rate'
+case `uname -s` in
+	Linux)
+		acpiout=`acpi`
+		if [ -n "$(echo -n ${acpiout} | grep 'charging at zero rate - will never fully charge.')" ]
+		then
+			echo -n "${acpiout}" | awk '{print $4, $5}' | awk -F',' '{print $1}' | sed -e 's@,@/@g' -e 's@ @@g' | tr -d '\n'
+			echo -n '/charging at zero rate'
 
-elif [ -n "$(echo -n ${acpiout} | grep 'Unknown, ')" ]
-then
-	echo -n "${acpiout}" | awk '{print $4, $5}' | sed -e 's@,@/@g' -e 's@ @@g' | tr -d '\n'
-	echo -n '/unknown'
+		elif [ -n "$(echo -n ${acpiout} | grep 'Unknown, ')" ]
+		then
+			echo -n "${acpiout}" | awk '{print $4, $5}' | sed -e 's@,@/@g' -e 's@ @@g' | tr -d '\n'
+			echo -n '/unknown'
 
-elif [ -n "$(echo -n ${acpiout} | grep 'Full, 100%')" ]
-then
-	echo -n "100%"
-else
-	echo -n "${acpiout}" | awk '{print $4, $5}' | sed 's@, @/@g' | awk -F: '{print $1 ":" $2}' | tr -d '\n'
-fi
+		elif [ -n "$(echo -n ${acpiout} | grep 'Full, 100%')" ]
+		then
+			echo -n "100%"
+		else
+			echo -n "${acpiout}" | awk '{print $4, $5}' | sed 's@, @/@g' | awk -F: '{print $1 ":" $2}' | tr -d '\n'
+		fi
+	;;
+	FreeBSD)
+		acpiout=`acpiconf -i 0`
+		# charging at zero rate
+	
+		# unknown
+		if echo "${acpiout}" | egrep -q '^Remaining time:[	]*unknown'
+		then
+			echo -n "${acpiout}" | sed -e '/^Remaining capacity/!d' \
+				-e 's/.*:[	]*//' | tr -d '\n'
+			echo -n '/unknown'
+		# full
+		elif echo "${acpiout}" | egrep -q '^Remaining capacity:[  ]*100%'
+		then
+			echo -n '100%'
+		# anything else
+		else
+			echo -n "${acpiout}" | sed -e '/^Remaining capacity/!d' \
+				-e 's/.*:[	]*//' | tr -d '\n'
+			echo -n "${acpiout}" | sed -e '/^Remaining time/!d' \
+				-e 's@Remaining time:[	]*@/@' | tr -d '\n'
+		fi
+	;;
+esac
 
 if $COLOR
 then
